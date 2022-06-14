7c25e4d5ecda54a9fe2e20b22ea274f149524eef
sting <a2437463@126.com>
2022-06-14 21:57:09 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::fs::File;
+use std::io::{ Read};
+use actix_web::{get, App, HttpServer, Responder};
+use actix_web::http::header::CONTENT_TYPE;
+
+#[get("/")]
+async fn index() -> impl Responder {
+    let path = "data.json";
+
+    let mut input = File::open(path).unwrap();
+
+    let mut buf = String::new();
+
+    input.read_to_string(&mut buf).unwrap();
+
+    buf.customize().insert_header((CONTENT_TYPE, "application/json"))
+    
+
+}
+
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>  {
+    HttpServer::new(|| {
+        App::new().service(index)
+    })
+    .bind(("127.0.0.1", 8060))?
+    .run()
+    .await
+}
